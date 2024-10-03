90d395b61ef2d4a831efd0250ca211c72e638b04
Orayanee Janjaeng <orayaneejj@gmail.com>
2024-10-03 12:23:24 +0700

    answer: ex1-6

--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,14 @@
 // Start coding here
+//Expression
+let exAdd = function (a,b) {
+    return Number(a) + Number(b);
+}
+let exSubtract = function (a,b) {
+    return Number(a) - Number(b);
+}
+let exMultiply = function (a,b) {
+    return Number(a) * Number(b);
+}
+let exDivide = function (a,b) {
+    return Number(a) / Number(b);
+}
--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,6 @@
 // Start coding here
+//Arrow
+let arrowAdd = (a,b) => Number(a) + Number(b);
+let arrowSubtract = (a,b) => Number(a) - Number(b);
+let arrowMultiply = (a,b) => Number(a) * Number(b);
+let arrowDivide = (a,b) => Number(a) / Number(b);
